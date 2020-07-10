32fb3648447ddbacf40acbce8f77215092082406
John Wong <johnwong@Johns-MacBook-Pro-2.local>
2020-07-10 14:54:38 -0700

    revise home page

--- src/containers/home-page/index.jsx
+++ src/containers/home-page/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import './home-page.scss';
 
 export default function HomePage() {
-    return <div>home page</div>
+    return (
+        <div className="home-page-container">
+            <h1>Welcome to MEC Coding Challenge. Plase use the search bar to search for products.</h1>
+        </div>
+    )
 }
